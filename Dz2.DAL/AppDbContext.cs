d4ac406ee22e131829ef840f84352cebe62b45c4
Adamc_jy76@student.itstep.org <Adamc_jy76@student.itstep.org>
2025-04-21 14:50:38 +0300

    Add project files.

---
+++ Dz2.DAL/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Dz2.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dz2
+{
+     public  class AppDbContext : DbContext
+    {
+        private string _conectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=UN;Integrated Security=True;Connect Timeout=30;";
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_conectionString);
+        }
+    }
+}
---
+++ Dz2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Lesson2;
+
+namespace Dz2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new ServiceMenu();
+
+            service.Start();
+        }
+    }
+}
